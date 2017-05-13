025d8f3b5eab7aaf4fdd49272fb8ba875b6de745
Joseph Yi <joe@josephyi.com>
2017-05-12 23:10:21 -0700

    remove test async await, set explicit node version for babel

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
-import Alexa from 'alexa-sdk'
+import Alexa from 'alexa-sdk';
 
 const handlers = {
-    'LaunchRequest': function () {
-        this.emit('HelloWorldIntent');
-    },
+  LaunchRequest: function() {
+    this.emit('HelloWorldIntent');
+  },
 
-    'HelloWorldIntent': function () {
-        this.emit(':tell', 'Hello World!');
-    }
- };
-
- const asyncFun = async  () => {
-  const value = await Promise
-    .resolve(1)
-    .then(x => x * 3)
-    .then(x => x + 5)
-    .then(x => x / 2);
-  return value;
-}
+  HelloWorldIntent: function() {
+    this.emit(':tell', 'Hello World!');
+  },
+};
 
 exports.handler = (event, context) => {
-  asyncFun().then(x => console.log(`x: ${x}`));
   const alexa = Alexa.handler(event, context);
   alexa.registerHandlers(handlers);
   alexa.execute();
-}
+};
