788b8e0afc846357c56320f92b2b72a8a85c4973
Joseph Yi <joe@josephyi.com>
2017-05-15 00:28:38 -0700

    basic free champion list skill

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import Alexa from 'alexa-sdk';
+import RiotAPI from './integration/RiotAPI';
 
 const handlers = {
   LaunchRequest: function() {
-    this.emit('HelloWorldIntent');
+    this.emit('FreeToPlayIntent');
   },
 
-  HelloWorldIntent: function() {
-    this.emit(':tell', 'Hello World!');
-  },
+  FreeToPlayIntent: async function() {
+    const api = new RiotAPI('RGAPI-662ebe32-5363-4299-9ae0-485342bfd504');
+    const [freeChamps, staticChamps] = await Promise.all([api.freeChampions(), api.staticChamps()]);
+    const names = freeChamps['champions'].map( (freeChamp) => staticChamps['data'][freeChamp['id']]['name']);
+    this.emit(':tell', `Here's who's free to play: ${names.join(', ')}.`);
+  }
 };
 
 exports.handler = (event, context) => {
---
+++ src/integration/RiotAPI.js
@@ -0,0 +1,29 @@
+const rp = require('request-promise-native');
+
+export default class RiotAPI {
+  constructor(key) {
+    this.headers = { 'X-Riot-Token': key };
+  }
+
+  freeChampions() {
+    return rp({
+      uri: 'https://na1.api.riotgames.com/lol/platform/v3/champions',
+      qs: {
+        freeToPlay: true
+      },
+      headers: this.headers,
+      json: true
+    });
+  }
+
+  staticChamps() {
+    return rp({
+      uri: 'https://na1.api.riotgames.com/lol/static-data/v3/champions',
+      qs: {
+        dataById: true
+      },
+      headers: this.headers,
+      json: true
+    })
+  }
+}
